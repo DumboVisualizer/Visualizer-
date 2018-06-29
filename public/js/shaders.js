8209c9151ad34f9674c0c384f1b949c5b81cfb29
unknown <geo3125@gmail.com>
2018-06-29 12:39:42 -0700

    changes before merge

--- public/js/shaders.js
+++ public/js/shaders.js
@@ -20,6 +20,7 @@ uniform float userInput_3;
 uniform float userInput_4;
 uniform float userInput_5;
 uniform float userInput_6;
+uniform float userInput_7;
 
 void main(void) {
   vec3 c;
@@ -30,7 +31,7 @@ void main(void) {
   float l = 0.2 * length(p);
   for (int i = 0; i < 3; i++) {
     z += 0.07;
-    uv += p / l * (sin(z) + userInput_1) * abs(sin(l * userInput_2 - z * userInput_3)) * (loudness * 0.01);
+    uv += p / l * (sin(z) + userInput_1) * abs(sin(l * userInput_2 - z * userInput_3)) * (loudness * userInput_7);
     c[i] = 0.01 / length(abs(mod(uv, userInput_4) - userInput_5));
   }
   float intensity = texture2D(spectrum, vec2(l, userInput_6)).x;
